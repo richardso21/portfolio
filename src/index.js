704d5c1063b4f0be7b5dbf1a4564af0bff635ee8
richardso21 <richard.so21@sitechhs.com>
2020-08-21 10:02:22 -0400

    mainText styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-// const Swup = require('swup').default;
-// const SwupOverlayTheme = require('@swup/overlay-theme');
+const Swup = require('swup').default;
+const SwupOverlayTheme = require('@swup/overlay-theme');
 
-// const swup = new Swup({
-//   plugins: [new SwupOverlayTheme()],
-// });
+const swup = new Swup({
+  plugins: [new SwupOverlayTheme({
+      direction: 'to-bottom'
+  })],
+});
